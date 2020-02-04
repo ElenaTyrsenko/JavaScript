8072674f44f184e496f0b3602ee047f68222b900
Elena <tyrsenkoo@gmail.com>
2020-02-04 22:08:22 +0200

    hometask
    hometask

--- Less05_09.01.20/Task1/ht5task1.js
+++ Less05_09.01.20/Task1/ht5task1.js
@@ -1,29 +1,6 @@
-// let  x = prompt('Введите число возводимое в степень');
-// let  n = prompt('Введите показатель степени');
-// function res(x, n) {
-//  if ( n == 1 ) {
-//    return x;
-//    } else {
-//      return x * res(x, n-1);
-//    }
-//   }
-//   alert( res(x, n) );
-
-// function res( x, n ) {
-//  return (n == 1) ? x : (x * res(x, n-1));
-// }
-// alert( 'Полученный результат' + '-' + res(x, n) );
 
 let  number = prompt('Введите число возводимое в степень');
 let  exponent = prompt('Введите показатель степени');
-// function result( number, exponent) {
-//  if ( exponent == 1 ) {
-//    return number;
-//    } else {
-//      return number * result(number, exponent-1);
-//    }
-//   }
-//   alert( result(number, exponent) );
 
 function result( number, exponent ) {
  return (exponent == 1) ? number : (number * result(number, exponent-1));
